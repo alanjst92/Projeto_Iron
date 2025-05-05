dec983dc9c91b9959b48d3db8f3645122787d7db
alanjst92 <alansan92@gmail.com>
2025-05-05 20:57:21 -0300

    primeiro commit

---
+++ iron/src/main/java/com/example/iron/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.iron;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "lista-clientes";
+    }
+}
